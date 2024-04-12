5eff60a1f93d46c206241156559d8775244e3f1d
Sirelkhatim Gamal <159180908+odooetqan@users.noreply.github.com>
2024-04-12 09:05:44 +0300

    Update partner.py

--- print_contract0001/model/partner.py
+++ print_contract0001/model/partner.py
@@ -19,19 +19,19 @@ class Partner(models.Model):
         partner_rec = self.env['res.partner'].search(
             [('name', '=', self.name), ('id', '!=', self.id)]) 
         if partner_rec:
-            raise ValueError(_('مكرر  ! هذا العميل موجود من قبل في جهات الاتصال .'))
+            raise UserError(_('مكرر  ! هذا العميل موجود من قبل في جهات الاتصال .'))
     @api.constrains('mobile')
     def _check_mobile(self):
         partner_rec = self.env['res.partner'].search(
             [('mobile', '=', self.mobile), ('id', '!=', self.id)]) 
         if partner_rec:
-            raise ValueError(_('مكرر ! هذا الرقم موجود في احدي جهات الاتصال '))
+            raise UserError(_('مكرر ! هذا الرقم موجود في احدي جهات الاتصال '))
     @api.constrains('id_number')
     def _check_mobile(self):
         partner_rec = self.env['res.partner'].search(
             [('id_number', '=', self.id_number), ('id', '!=', self.id)]) 
         if partner_rec:
-            raise ValueError(_('مكرر ! هذا الهوية موجود في احدي جهات الاتصال '))
+            raise UserError(_('مكرر ! هذا الهوية موجود في احدي جهات الاتصال '))
 
 # On Tue, 26 Nov 2019 at 11:17, shalin wilson <shalinwilson1994@gmail.com> wrote:
     _sql_constraints = [('phone', 'unique(phone)', 'Error Message')]
