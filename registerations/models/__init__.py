6832dcfa360de0cdf7221aa26ad2fd8b5bb3e1d3
Sirelkhatim Gamal <159180908+odooetqan@users.noreply.github.com>
2024-04-13 18:54:33 +0300

    Delete registerations directory

--- registerations/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import configuration
-from . import student_guardian
-from . import student_student
-
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
