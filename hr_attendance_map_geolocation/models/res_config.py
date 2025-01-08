491af163d3a6f5296f3b6bc2675f9d2635261b04
Sirelkhatim Gamal Sirelkhatim Musa <odoo@etqan-ltd.com>
2025-01-08 14:27:10 +0300

    update contract history 6

--- hr_attendance_map_geolocation/models/res_config.py
+++ hr_attendance_map_geolocation/models/res_config.py
@@ -4,7 +4,13 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    attendance_location = fields.Integer(string='Attendance Location')
+
+    attendance_location = fields.Float(
+        string="Attendance Location",
+        config_parameter='hr_attendance.attendance_location',
+        help="Set the maximum distance for attendance location to allow sign-in/sign-out."
+    )
+    # attendance_location = fields.Integer(string='Attendance Location')
 
     @api.model
     def get_values(self):
@@ -16,3 +22,9 @@ class ResConfigSettings(models.TransientModel):
     def set_values(self):
         self.env['ir.config_parameter'].sudo().set_param('hr_attendance_map_geolocation.attendance_location', self.attendance_location)
         super(ResConfigSettings, self).set_values()
+
+# from odoo import fields, models
+
+# class ResConfigSettings(models.TransientModel):
+#     _inherit = 'res.config.settings'
+
