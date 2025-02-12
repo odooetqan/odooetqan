f8c3d9425c06ba3e842bb8ac092743872a7a7407
seralkhatem3210 <seralkhatem3210@gmail.com>
2025-02-12 14:00:31 +0300

    add an portal addons

---
+++ portal_custom_account/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Portal Custom Account',
+    'version': '1.0',
+    'category': 'Portal',
+    'summary': 'Custom portal account page for users',
+    'author': 'Your Name',
+    'depends': ['portal', 'hr'],
+    'data': [
+        'views/portal_account_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
+    
