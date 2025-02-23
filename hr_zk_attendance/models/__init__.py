982ef3f55f51e069a7e5a97a15a5bee6db248e3a
Sirelkhatim Gamal <159180908+odooetqan@users.noreply.github.com>
2025-02-23 22:24:21 +0300

    Update __init__.py

--- hr_zk_attendance/models/__init__.py
+++ hr_zk_attendance/models/__init__.py
@@ -23,3 +23,5 @@ from . import biometric_device_details
 # from . import zk_machine_attendance
 from . import daily_attendance
 from . import hr_employee
+from . import hr_attendance
+from . import hr_payslip
