2f0c432787ccc33665b5a89a771c5929c4ffdd46
Sirelkhatim Gamal <159180908+odooetqan@users.noreply.github.com>
2025-02-28 14:05:01 +0300

    Delete hr_zk_attendance directory

--- hr_zk_attendance/models/setting.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    replace_standard_wizard = fields.Boolean(string="Replace Standard Wizard")
-
