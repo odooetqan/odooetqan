34f83709cb32170a1a614158b364bf14e1d59c00
seralkhatem3210 <seralkhatem3210@gmail.com>
2025-03-03 02:36:11 +0300

    updated

--- hr_zk_attendance_update/__manifest__.py
+++ hr_zk_attendance_update/__manifest__.py
@@ -31,7 +31,7 @@
     'company': 'Cybrosys Techno Solutions',
     'maintainer': 'Cybrosys Techno Solutions',
     'website': "https://www.cybrosys.com",
-    'depends': ['base_setup', 'hr_attendance','hr_payroll', 'base', 'mail',],
+    'depends': ['base_setup', 'hr_attendance','hr_payroll','hr', 'base', 'mail',],
 
 'depends': [
     # other dependencies
