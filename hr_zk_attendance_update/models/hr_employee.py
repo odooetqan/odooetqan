137154066ea668c5c5a1068d864bc0275f3ae045
seralkhatem3210 <seralkhatem3210@gmail.com>
2025-03-03 02:53:28 +0300

    updated

--- hr_zk_attendance_update/models/hr_employee.py
+++ hr_zk_attendance_update/models/hr_employee.py
@@ -26,8 +26,20 @@ class HrEmployee(models.Model):
     """Inherit the model to add field"""
     _inherit = 'hr.employee'
 
+
     device_id_num = fields.Char(string='Biometric Device ID',
                                 help="Give the biometric device id")
+    deduction_multiplier = fields.Float(string='Deduction Multiplier', default=1.0)
+    allowance_multiplier = fields.Float(string='Allowance Multiplier', default=1.0)
+    per_minute_rate = fields.Float(string='Per Minute Rate', compute='_compute_per_minute_rate')
+
+
+    @api.depends('contract_id', 'contract_id.wage', 'contract_id.resource_calendar_id.hours_per_day')
+    def _compute_per_minute_rate(self):
+        for employee in self:
+            if employee.contract_id and employee.contract_id.resource_calendar_id:
+                daily_minutes = employee.contract_id.resource_calendar_id.hours_per_day * 60
+                employee.per_minute_rate = employee.contract_id.wage / daily_minutes if daily_minutes else 0.0
 
 
 class HrContractHistory(models.Model):
