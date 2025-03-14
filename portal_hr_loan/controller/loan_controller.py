9338c37585c58c90cfe4952fa2657685b520e3b0
seralkhatem3210 <seralkhatem3210@gmail.com>
2025-03-15 00:58:44 +0300

    Portal attendance update

--- portal_hr_loan/controller/loan_controller.py
+++ portal_hr_loan/controller/loan_controller.py
@@ -43,29 +43,47 @@ class PortalHrLoan(http.Controller):
             'state': 'draft',
         })
         return request.redirect('/my/loans')
+
+
+@http.route('/my/salaries', type='http', auth="user", website=True)
+def portal_salaries(self, **kwargs):
+    employee = request.env['hr.employee'].sudo().search([('user_id', '=', request.env.user.id)], limit=1)
     
+    _logger.info("User ID: %s", request.env.user.id)
+    _logger.info("Employee ID: %s", employee.id if employee else "No employee found")
+    
+    if not employee:
+        return request.render('portal_hr_loan.portal_hr_salary_list', {'salaries': []})
+    
+    salaries = request.env['hr.salary'].sudo().search([('employee_id', '=', employee.id)])
+
+    _logger.info("Salaries: %s", salaries)
+
+    return request.render('portal_hr_loan.portal_hr_salary_list', {'salaries': salaries})
 
 
 
 
 
-class PortalHrSalary(http.Controller):
 
-    @http.route('/my/salaries', type='http', auth="user", website=True)
-    def portal_salaries(self, **kwargs):
-        salaries = request.env['hr.salary'].sudo().search([('employee_id.user_id', '=', request.env.user.id)])
-        return request.render('portal_hr_loan.portal_hr_salary_list', {'salaries': salaries})
 
-    @http.route('/my/salaries/new', type='http', auth="user", website=True)
-    def portal_create_salary(self, **kwargs):
-        return request.render('portal_hr_loan.portal_create_salary_form', {})
+# class PortalHrSalary(http.Controller):
 
-    @http.route('/my/salaries/save', type='http', auth="user", methods=['POST'], website=True, csrf=True)
-    def portal_save_salary(self, **kwargs):
-        request.env['hr.salary'].sudo().create({
-            'employee_id': request.env.user.employee_id.id,
-            'amount': kwargs.get('amount'),
-            'description': kwargs.get('description'),
-            'state': 'draft',
-        })
-        return request.redirect('/my/salaries')
+#     @http.route('/my/salaries', type='http', auth="user", website=True)
+#     def portal_salaries(self, **kwargs):
+#         salaries = request.env['hr.salary'].sudo().search([('employee_id.user_id', '=', request.env.user.id)])
+#         return request.render('portal_hr_loan.portal_hr_salary_list', {'salaries': salaries})
+
+#     @http.route('/my/salaries/new', type='http', auth="user", website=True)
+#     def portal_create_salary(self, **kwargs):
+#         return request.render('portal_hr_loan.portal_create_salary_form', {})
+
+#     @http.route('/my/salaries/save', type='http', auth="user", methods=['POST'], website=True, csrf=True)
+#     def portal_save_salary(self, **kwargs):
+#         request.env['hr.salary'].sudo().create({
+#             'employee_id': request.env.user.employee_id.id,
+#             'amount': kwargs.get('amount'),
+#             'description': kwargs.get('description'),
+#             'state': 'draft',
+#         })
+#         return request.redirect('/my/salaries')
