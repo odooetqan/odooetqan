94d06208dd6e10ffd3ca1bc5697d03446e77666d
seralkhatem3210 <seralkhatem3210@gmail.com>
2025-03-24 22:41:14 +0300

    update portal hr

--- einv_sa/model/base_document_layout.py
+++ einv_sa/model/base_document_layout.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError, Warning
+from odoo.exceptions import ValidationError, Warning
+from odoo.exceptions import UserError
 
 
 class BaseDocumentLayout(models.TransientModel):
