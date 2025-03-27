5856b2e743594d839b32ca3158159f54dca69fb2
uss-alilm <uss_alilm@hotmail.com>
2025-03-27 20:38:53 +0300

    update portal approval

--- portal_approval/models/portal_approval.py
+++ portal_approval/models/portal_approval.py
@@ -1,10 +1,8 @@
 from odoo import models, fields, api
-from odoo import models, fields, api
 
 class PortalApprovalCategory(models.Model):
     _inherit = "approval.category"
 
-    # Add custom fields to the approval.category model
     has_date = fields.Boolean(string="يحتوي على التاريخ")
     has_document = fields.Boolean(string="يحتوي على المستند")
     has_amount = fields.Boolean(string="يحتوي على المبلغ")
@@ -13,18 +11,12 @@ class PortalApprovalCategory(models.Model):
 class PortalApprovalRequest(models.Model):
     _inherit = "approval.request"
 
-    # Extend approval request with additional fields to be filled by the portal
     date_from = fields.Datetime(string="من")
     date_to = fields.Datetime(string="إلى")
     document = fields.Binary(string="المستند")
     amount = fields.Float(string="المبلغ")
 
-
-class PortalApproval(models.Model):
-    _inherit = "hr.leave.type"
-
-    overtime_deductible = fields.Boolean(string="Overtime Deductible", default=False)
-
-
-class HrLoan(models.Model):
-    _inherit = 'hr.loan'
+    # Related fields from category
+    has_date = fields.Boolean(related='category_id.has_date', string="يحتوي على التاريخ", store=True)
+    has_document = fields.Boolean(related='category_id.has_document', string="يحتوي على المستند", store=True)
+    has_amount = fields.Boolean(related='category_id.has_amount', string="يحتوي على المبلغ", store=True)
