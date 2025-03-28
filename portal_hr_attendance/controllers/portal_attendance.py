a7f99cc87a0cfc13c29995c7bd679a1cccfef52c
uss-alilm <uss_alilm@hotmail.com>
2025-03-29 00:43:47 +0300

    init hr attendanc  portal

--- portal_hr_attendance/controllers/portal_attendance.py
+++ portal_hr_attendance/controllers/portal_attendance.py
@@ -4,7 +4,7 @@ import pytz
 import json
 
 class PortalAttendanceController(http.Controller):
-    @http.route(['/my/attendance'], type='http', auth='user', website=True)
+    @http.route(['/my/attendance_check'], type='http', auth='user', website=True)
     def portal_my_attendance(self, **kwargs):
         """
         Display an attendance page with a "Check In" or "Check Out" button
