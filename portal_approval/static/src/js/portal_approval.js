bcbfd64f07a88f4cd00743aaa63be756927673df
uss-alilm <uss_alilm@hotmail.com>
2025-04-01 23:40:12 +0300

    update portal portal HR diduction

---
+++ portal_approval/static/src/js/portal_approval.js
@@ -0,0 +1,34 @@
+odoo.define('portal_approval.portal_approval', function (require) {
+    'use strict';
+
+    const publicWidget = require('web.public.widget');
+
+    publicWidget.registry.PortalApprovalForm = publicWidget.Widget.extend({
+        selector: '#category_id',
+        events: {
+            'change': '_onCategoryChange',
+        },
+
+        _onCategoryChange: function (ev) {
+            const categoryId = $(ev.target).val();
+            if (!categoryId) {
+                $('#dynamic_fields').empty();
+                return;
+            }
+
+            $.ajax({
+                url: '/my/approval/get_dynamic_fields',
+                dataType: 'json',
+                data: { category_id: categoryId },
+                success: function (res) {
+                    if (res.success && res.html) {
+                        $('#dynamic_fields').html(res.html);
+                    }
+                },
+                error: function (err) {
+                    console.error('Dynamic field load failed:', err);
+                }
+            });
+        },
+    });
+});
