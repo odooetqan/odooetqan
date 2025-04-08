cac70fdc423347c7cfa68eb1bb93d02cef5fd1dd
uss-alilm <uss_alilm@hotmail.com>
2025-04-08 18:39:16 +0300

    update portal portal HR

--- portal_hr_loan/controller/loan_controller.py
+++ portal_hr_loan/controller/loan_controller.py
@@ -10,7 +10,7 @@ class PortalHrLoan(http.Controller):
     @http.route('/my/loans', type='http', auth="user", website=True)
     def portal_loans(self, **kwargs):
         loans = request.env['hr.loan'].sudo().search([('employee_id.user_id', '=', request.env.user.id)])
-        return request.render('portal_hr_loan.portal_hr_loan_list', {'loans': loans})
+        return request.render('portal_hr_loan.portal_hr_loan_tree', {'loans': loans})
 
     @http.route('/my/loans/new', type='http', auth="user", website=True)
     def portal_create_loan(self, **kwargs):
@@ -35,10 +35,10 @@ class PortalHrLoan(http.Controller):
         _logger.info("Employee ID: %s", employee.id if employee else "No employee found")
         
         if not employee:
-            return request.render('portal_hr_loan.portal_hr_salary_list', {'salaries': []})
+            return request.render('portal_hr_loan.portal_hr_salary_tree', {'salaries': []})
         
         salaries = request.env['hr.payslip'].sudo().search([('employee_id', '=', employee.id)])
 
         _logger.info("Salaries: %s", salaries)
 
-        return request.render('portal_hr_loan.portal_hr_salary_list', {'salaries': salaries})
+        return request.render('portal_hr_loan.portal_hr_salary_tree', {'salaries': salaries})
