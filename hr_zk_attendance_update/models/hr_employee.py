29303be5ddab6a73dcf42294460d86392f0d022a
uss-alilm <uss_alilm@hotmail.com>
2025-04-19 00:44:51 +0300

    update late to late_minutes

--- hr_zk_attendance_update/models/hr_employee.py
+++ hr_zk_attendance_update/models/hr_employee.py
@@ -31,6 +31,7 @@ class HrEmployee(models.Model):
     deduction_multiplier = fields.Float(string='Deduction Multiplier', default=1.0)
     allowance_multiplier = fields.Float(string='Allowance Multiplier', default=1.0)
     per_minute_rate = fields.Float(string='Per Minute Rate', compute='_compute_per_minute_rate', default=0.0)
+    per_minute_rate_manual = fields.Float(string='Per Minute Rate Manual',  default=0.0)
 
     # @api.depends('contract_id', 'contract_id.wage', 'contract_id.resource_calendar_id.hours_per_day')
     # def _compute_per_minute_rate(self):
@@ -45,13 +46,15 @@ class HrEmployee(models.Model):
         for employee in self:
             # تعيين القيمة الافتراضية دائمًا لضمان عدم حدوث الخطأ
             employee.per_minute_rate = 0.0  
+            if employee.per_minute_rate_manual:
+                employee.per_minute_rate = employee.per_minute_rate_manual
+            else:
+                if employee.contract_id and employee.contract_id.wage and employee.contract_id.resource_calendar_id:
+                    hours_per_day = employee.contract_id.resource_calendar_id.hours_per_day or 8
+                    daily_minutes = hours_per_day * 60 * 30
 
-            if employee.contract_id and employee.contract_id.wage and employee.contract_id.resource_calendar_id:
-                hours_per_day = employee.contract_id.resource_calendar_id.hours_per_day or 8
-                daily_minutes = hours_per_day * 60 * 30
-
-                if daily_minutes > 0:
-                    employee.per_minute_rate = employee.contract_id.wage / daily_minutes
+                    if daily_minutes > 0:
+                        employee.per_minute_rate = employee.contract_id.wage / daily_minutes
 
 
 class HrContractHistory(models.Model):
